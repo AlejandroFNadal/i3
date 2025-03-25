3f197254d7bda5047948473e703e331b0e82d1d8
alejandro.nadal@synics.ch <alejandro.nadal@synics.ch>
2025-03-25 10:28:28 +0100

    certain apps are tied to certain workspaces on startup

--- monitor_setup.sh
+++ monitor_setup.sh
@@ -3,9 +3,12 @@
 # Detect connected monitors
 if xrandr | grep "HDMI-1 connected"; then
     # Set up the external monitor to the right of your primary display (e.g., eDP-1)
-    xrandr --output HDMI-1 --auto --right-of eDP-1
+    xrandr --output HDMI-1 --auto --left-of eDP-1
 else
     # If HDMI-1 is disconnected, disable it
     xrandr --output HDMI-1 --off
+fi
+if xrandr | grep "DP-1 connected"; then
+    xrandr --output DP-1 --auto --left-of HDMI-1
 fi
 
